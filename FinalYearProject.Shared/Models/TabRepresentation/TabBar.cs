604d94762927d4aaf52c5e236ae1e64ce6e50839
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-10-08 16:22:58 +0100

    Added representations of musical Information

---
+++ FinalYearProject.Shared/Models/TabRepresentation/TabBar.cs
@@ -0,0 +1,25 @@
+﻿
+namespace FinalYearProject.Shared.Models.TabRepresentation
+{
+
+    /// <summary>
+    /// Representation of a Bar of music in a tablature format
+    /// </summary>
+    public class TabBar
+    {
+        /// <summary>
+        /// The total number of beats in this bar
+        /// </summary>
+        public required TimeSignature TimeSignature { get; set; }
+
+        /// <summary>
+        /// The number of this bar in the tablature, starting from 1
+        /// </summary>
+        public int BarNumber { get; set; }
+
+        /// <summary>
+        /// Collection of all the notes in this bar
+        /// </summary>
+        public required List<TabNote> Notes { get; set; } = [];
+    }
+}
---
+++ FinalYearProject.Shared/Models/TabRepresentation/TabNote.cs
@@ -0,0 +1,28 @@
+﻿namespace FinalYearProject.Shared.Models.TabRepresentation
+{
+    /// <summary>
+    /// A single note in a tablature representation
+    /// </summary>
+    public class TabNote
+    {
+        /// <summary>
+        /// 1 = high E, 6 = low E, If null then it is a rest
+        /// </summary>
+        public required int? StringNumber { get; set; }
+
+        /// <summary>
+        /// The fret for this note
+        /// </summary>
+        public required int FretNumber { get; set; }
+
+        /// <summary>
+        /// When the note started in Beats
+        /// </summary>
+        public required double StartTime { get; set; }
+
+        /// <summary>
+        /// How long the note will last in Beats
+        /// </summary>
+        public required double Duration { get; set; }
+    }
+}
