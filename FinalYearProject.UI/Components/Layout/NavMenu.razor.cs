7eb88f21929fae2a3d4115059730264ac4ff8590
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-10-08 16:07:36 +0100

    Add project files.

---
+++ FinalYearProject.UI/Components/Layout/NavMenu.razor.cs
@@ -0,0 +1,19 @@
+namespace FinalYearProject.UI.Components.Layout
+{
+    public partial class NavMenu
+    {
+        private bool collapseNavMenu = true;
+
+        private string? NavMenuCssClass => collapseNavMenu ? "collapse" : null;
+
+        private void ToggleNavMenu()
+        {
+            collapseNavMenu = !collapseNavMenu;
+        }
+
+        private void CollapseNavMenu()
+        {
+            collapseNavMenu = true;
+        }
+    }
+}
---
+++ FinalYearProject.UI/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FinalYearProject.UI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
