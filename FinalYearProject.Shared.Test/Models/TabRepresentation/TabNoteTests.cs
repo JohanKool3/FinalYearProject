fcf5dd070902df3b32c4fda81b103d815e750415
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-10-08 16:43:22 +0100

    Added unit tests for TabNote

---
+++ FinalYearProject.Shared.Test/Models/TabRepresentation/TabNoteTests.cs
@@ -0,0 +1,88 @@
+﻿using FinalYearProject.Shared.Models.TabRepresentation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalYearProject.Shared.Test.Models.TabRepresentation
+{
+    public class TabNoteTests
+    {
+
+        [Fact]
+        public void TabNote_Constructor_SetsPropertiesCorrectly()
+        {
+            // Arrange
+            int stringNumber = 3;
+            int fretNumber = 5;
+            int startTime = 0;
+            int duration = 4;
+            
+            // Act
+            var tabNote = new TabNote(stringNumber, fretNumber,startTime, duration);
+            
+            // Assert
+            Assert.Equal(stringNumber, tabNote.StringNumber);
+            Assert.Equal(fretNumber, tabNote.FretNumber);
+            Assert.Equal(duration, tabNote.Duration);
+        }
+
+        [Theory]
+        [InlineData(0)]  // Invalid string number (too low)
+        [InlineData(7)]  // Invalid string number (too high)
+        public void TabNote_Constructor_InvalidStringNumber_ThrowsArgumentOutOfRangeException(int invalidStringNumber)
+        {
+            // Arrange
+            int fretNumber = 5;
+            int startTime = 0;
+            int duration = 4;
+
+            // Act & Assert
+            Assert.Throws<ArgumentOutOfRangeException>(() 
+                => new TabNote(invalidStringNumber, fretNumber, startTime, duration));
+        }
+
+        [Theory]
+        [InlineData(-1)]  // Invalid fret number (negative)
+        [InlineData(31)]  // Invalid fret number (too high)
+        public void TabNote_Constructor_InvalidFretNumber_ThrowsArgumentOutOfRangeException(int invalidFretNumber)
+        {
+            // Arrange
+            int stringNumber = 3;
+            int startTime = 0;
+            int duration = 4;
+            // Act & Assert
+            Assert.Throws<ArgumentOutOfRangeException>(() 
+                => new TabNote(stringNumber, invalidFretNumber, startTime, duration));
+        }
+
+        [Fact]
+        public void TabNote_Constructor_NegativeStartTime_ThrowsArgumentOutOfRangeException()
+        {
+            // Arrange
+            int stringNumber = 3;
+            int fretNumber = 5;
+            int invalidStartTime = -1;
+            int duration = 4;
+            // Act & Assert
+            Assert.Throws<ArgumentOutOfRangeException>(() 
+                => new TabNote(stringNumber, fretNumber, invalidStartTime, duration));
+        }
+
+        [Theory]
+        [InlineData(0)]  // Invalid duration (zero)
+        [InlineData(-2)] // Invalid duration (negative)
+        public void TabNote_Constructor_InvalidDuration_ThrowsArgumentOutOfRangeException(int invalidDuration)
+        {
+            // Arrange
+            int stringNumber = 3;
+            int fretNumber = 5;
+            int startTime = 0;
+
+            // Act & Assert
+            Assert.Throws<ArgumentOutOfRangeException>(() 
+                => new TabNote(stringNumber, fretNumber, startTime, invalidDuration));
+        }
+    }
+}
--- FinalYearProject.Shared/Models/TabRepresentation/TabNote.cs
+++ FinalYearProject.Shared/Models/TabRepresentation/TabNote.cs
@@ -5,24 +5,70 @@
     /// </summary>
     public class TabNote
     {
+        public TabNote(int stringNumber, int fretNumber, double startTime, double duration)
+        {
+            // TODO: Allow user to customize this to allow for 7 string guitars etc.
+            // Most likely this will need to be done through DI and a settings service
+            ValidateInput(stringNumber, fretNumber, startTime, duration);
+
+            StringNumber = stringNumber;
+            FretNumber = fretNumber;
+            StartTime = startTime;
+            Duration = duration;
+        }
+
+        /// <summary>
+        /// // Check Validity of Inputs
+        /// </summary>
+        /// <param name="stringNumber">String where this note will be played</param>
+        /// <param name="fretNumber">Fret where this note will be played</param>
+        /// <param name="startTime">When the note started (in beats)</param>
+        /// <param name="duration">How long this note will last</param>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        private static void ValidateInput(int stringNumber, int fretNumber, double startTime, double duration)
+        {
+            if (stringNumber < 0 || stringNumber > 6)
+            {
+                throw new ArgumentOutOfRangeException(nameof(stringNumber), "String number must be between 1 and 6, or null for a rest.");
+            }
+
+            if (fretNumber < 0 || fretNumber > 30)
+            {
+                throw new ArgumentOutOfRangeException(nameof(fretNumber), "Fret number must be between 0 and 24.");
+            }
+
+            if (startTime < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(startTime), "Start time must be greater than or equal to 0.");
+            }
+
+            if (duration <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(duration), "Duration must be greater than 0.");
+            }
+        }
+
         /// <summary>
         /// 1 = high E, 6 = low E, If null then it is a rest
         /// </summary>
-        public required int? StringNumber { get; set; }
+        public int? StringNumber { get; private set; }
 
         /// <summary>
         /// The fret for this note
         /// </summary>
-        public required int FretNumber { get; set; }
+        public int FretNumber { get; private set; }
 
         /// <summary>
         /// When the note started in Beats
         /// </summary>
-        public required double StartTime { get; set; }
+        public double StartTime { get; private set; }
 
         /// <summary>
         /// How long the note will last in Beats
         /// </summary>
-        public required double Duration { get; set; }
+        public double Duration { get; private set; }
+
+        //  TODO: Implement Articulation and Note Qualities
+        //public required Articulation Articulation { get; set; }
     }
 }
