3dc2cc364bc293a999bdaa650b695b1b4c9ba99b
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-10-15 14:56:08 +0100

    Added basic representation for bar and time signature

--- FinalYearProject.UI/Components/Pages/TabView.razor.cs
+++ FinalYearProject.UI/Components/Pages/TabView.razor.cs
@@ -28,62 +28,62 @@ namespace FinalYearProject.UI.Components.Pages
                         BarNumber = 1,
                         TimeSignature = new TimeSignature(4,4)
                     },
-                    new MusicalBar(){
-                            Notes = [
-                                new MusicalNote(6, 1, 0, 0.5),
-                                new MusicalNote(6, 1, 0.5, 0.5),
-                                new MusicalNote(6, 1, 1, 0.5),
-                                new MusicalNote(6, 1, 1.5, 0.5),
-                                new MusicalNote(6, 1, 2, 0.5),
-                                new MusicalNote(6, 1, 2.5, 0.5),
-                                new MusicalNote(6, 1, 3, 0.5),
-                                new MusicalNote(6, 1, 3.5, 0.5),
-                            ],
-                            BarNumber = 2,
-                            TimeSignature = new TimeSignature(4,4)
-                        },
-                new MusicalBar(){
-                        Notes = [
-                            new MusicalNote(6, 3, 0, 1),
-                            new MusicalNote(6, 3, 1, 1),
-                            new MusicalNote(6, 3, 2, 1),
-                            new MusicalNote(6, 3, 3, 1),
-                            new MusicalNote(6, 3, 4, 1),
-                            new MusicalNote(6, 3, 5, 1),
-                        ],
-                        BarNumber = 3,
-                        TimeSignature = new TimeSignature(6,8)
-                        },
-                new MusicalBar(){
-                    Notes = [
-                        new MusicalNote(4, 2, 0, 1),
-                        new MusicalNote(4, 2, 1, 1),
-                        new MusicalNote(4, 2, 2, 1),
-                        new MusicalNote(4, 2, 3, 1),
-                        new MusicalNote(4, 2, 4, 1),
-                        new MusicalNote(4, 2, 5, 1),
-                        new MusicalNote(4, 2, 6, 1),
-                        new MusicalNote(4, 2, 7, 1),
-                        new MusicalNote(4, 2, 8, 1),
-                        new MusicalNote(4, 2, 9, 1),
-                        new MusicalNote(4, 2, 10, 1),
-                        new MusicalNote(4, 2, 11, 1),
-                        new MusicalNote(4, 2, 12, 1),
-                    ],
-                    BarNumber = 4,
-                    TimeSignature = new TimeSignature(16,13)
-                },
-                new MusicalBar(){
-                    Notes = [
-                        new MusicalNote(6, 0, 0, 1),
-                        new MusicalNote(5, 1, 1, 1),
-                        new MusicalNote(4, 2, 2, 1),
-                        new MusicalNote(3, 3, 3, 1),
-                        new MusicalNote(2, 4, 4, 1),
-                    ],
-                    BarNumber = 5,
-                    TimeSignature = new TimeSignature(5,4)
-                },
+                //    new MusicalBar(){
+                //            Notes = [
+                //                new MusicalNote(6, 1, 0, 0.5),
+                //                new MusicalNote(6, 1, 0.5, 0.5),
+                //                new MusicalNote(6, 1, 1, 0.5),
+                //                new MusicalNote(6, 1, 1.5, 0.5),
+                //                new MusicalNote(6, 1, 2, 0.5),
+                //                new MusicalNote(6, 1, 2.5, 0.5),
+                //                new MusicalNote(6, 1, 3, 0.5),
+                //                new MusicalNote(6, 1, 3.5, 0.5),
+                //            ],
+                //            BarNumber = 2,
+                //            TimeSignature = new TimeSignature(4,4)
+                //        },
+                //new MusicalBar(){
+                //        Notes = [
+                //            new MusicalNote(6, 3, 0, 1),
+                //            new MusicalNote(6, 3, 1, 1),
+                //            new MusicalNote(6, 3, 2, 1),
+                //            new MusicalNote(6, 3, 3, 1),
+                //            new MusicalNote(6, 3, 4, 1),
+                //            new MusicalNote(6, 3, 5, 1),
+                //        ],
+                //        BarNumber = 3,
+                //        TimeSignature = new TimeSignature(6,8)
+                //        },
+                //new MusicalBar(){
+                //    Notes = [
+                //        new MusicalNote(4, 2, 0, 1),
+                //        new MusicalNote(4, 2, 1, 1),
+                //        new MusicalNote(4, 2, 2, 1),
+                //        new MusicalNote(4, 2, 3, 1),
+                //        new MusicalNote(4, 2, 4, 1),
+                //        new MusicalNote(4, 2, 5, 1),
+                //        new MusicalNote(4, 2, 6, 1),
+                //        new MusicalNote(4, 2, 7, 1),
+                //        new MusicalNote(4, 2, 8, 1),
+                //        new MusicalNote(4, 2, 9, 1),
+                //        new MusicalNote(4, 2, 10, 1),
+                //        new MusicalNote(4, 2, 11, 1),
+                //        new MusicalNote(4, 2, 12, 1),
+                //    ],
+                //    BarNumber = 4,
+                //    TimeSignature = new TimeSignature(16,13)
+                //},
+                //new MusicalBar(){
+                //    Notes = [
+                //        new MusicalNote(6, 0, 0, 1),
+                //        new MusicalNote(5, 1, 1, 1),
+                //        new MusicalNote(4, 2, 2, 1),
+                //        new MusicalNote(3, 3, 3, 1),
+                //        new MusicalNote(2, 4, 4, 1),
+                //    ],
+                //    BarNumber = 5,
+                //    TimeSignature = new TimeSignature(5,4)
+                //},
 
                 ]
             };
