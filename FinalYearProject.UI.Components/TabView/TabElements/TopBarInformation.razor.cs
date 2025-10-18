782b0b1626804e8eaae0db9db2523e0db3fbce73
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-10-18 18:30:54 +0100

    Reworked top bar information to be dynamic

--- FinalYearProject.UI.Components/TabView/TabElements/TopBarInformation.razor.cs
+++ FinalYearProject.UI.Components/TabView/TabElements/TopBarInformation.razor.cs
@@ -1,25 +1,41 @@
+using FinalYearProject.UI.Components.Services;
 using Microsoft.AspNetCore.Components;
 
 namespace FinalYearProject.UI.Components.TabView.TabElements
 {
-    public partial class TopBarInformation
+    public partial class TopBarInformation(TabRepresentationSettingsService settingsService)
     {
+        public TabRepresentationSettingsService SettingsService { get; } 
+            = settingsService;
+
         #region Parameters
+        
         /// <summary>
         /// The Number of the bar to display.
         /// </summary>
         [Parameter]
         public int BarNumber { get; set; } = 0;
 
-        [Parameter]
-        public int LeftPadding { get; set; } = 5;
-
-        [Parameter]
-        public int TopPadding { get; set; } = 10;
-
-        [Parameter]
-        public int FontSize { get; set; } = 12;
-
         #endregion
+
+        /// <summary>
+        /// Top padding is half that of the notes area to indicate,
+        /// seperation
+        /// </summary>
+        private int _topPadding
+            => SettingsService.NoteDisplaySettings.TopPadding / 2;
+
+        /// <summary>
+        /// Left padding is a quarter that of the notes area to indicate,
+        /// seperation
+        /// </summary>
+        private int _leftPadding
+            => SettingsService.NoteDisplaySettings.LeftPadding / 4;
+
+        /// <summary>
+        /// Bar number is two thirds the size of the string spacing
+        /// </summary>
+        private int _barNumberFontSize
+            => (2* SettingsService.NoteDisplaySettings.StringSpacing) / 3;
     }
 }
