950d3f2f6cea34e414e83ad7954c73018c329d7d
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-10-19 16:10:29 +0100

    Commented barline component

--- FinalYearProject.UI.Components/InterfaceElements/Bar/BarLine.razor.cs
+++ FinalYearProject.UI.Components/InterfaceElements/Bar/BarLine.razor.cs
@@ -4,13 +4,21 @@ namespace FinalYearProject.UI.Components.InterfaceElements.Bar
 {
     public partial class BarLine
     {
-
+        /// <summary>
+        /// Where the Bar Line should be drawn on the X Axis
+        /// </summary>
         [Parameter, EditorRequired]
         public int XPosition { get; set; }
 
+        /// <summary>
+        /// Where the Bar Line should start on the Y Axis
+        /// </summary>
         [Parameter, EditorRequired]
         public int StartYPosition { get; set; }
 
+        /// <summary>
+        /// Where the Bar Line should end on the Y Axis
+        /// </summary>
         [Parameter, EditorRequired]
         public int EndYPosition { get; set; }
     }
