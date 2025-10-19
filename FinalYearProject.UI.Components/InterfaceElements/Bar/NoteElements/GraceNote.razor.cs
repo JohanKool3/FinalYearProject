05ed192510c41da5ca653692ea90d834c9314e91
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-10-19 16:16:04 +0100

    Seperated grace note defintion for simplicity and flexibility

---
+++ FinalYearProject.UI.Components/InterfaceElements/Bar/NoteElements/GraceNote.razor.cs
@@ -0,0 +1,38 @@
+using FinalYearProject.UI.Components.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace FinalYearProject.UI.Components.InterfaceElements.Bar.NoteElements
+{
+    public partial class GraceNote
+    {
+        /// <summary>
+        /// The Note to display.
+        /// </summary>
+        [Parameter, EditorRequired]
+        public required NoteDisplayInformation Note { get; set; }
+
+        /// <summary>
+        /// The X Position of the Note
+        /// </summary>
+        [Parameter]
+        public int XPosition { get; set; }
+
+        /// <summary>
+        /// The Y Position of the Note
+        /// </summary>
+        [Parameter]
+        public int YPosition { get; set; }
+
+        /// <summary>
+        /// How big the Note should be.
+        /// </summary>
+        [Parameter]
+        public int Size { get; set; }
+
+        /// <summary>
+        /// Whether to draw the outline of the Note.
+        /// </summary>
+        [Parameter]
+        public bool DrawOutline { get; set; } = true;
+    }
+}
