e0a754fc1989abf808a42ebf794495bde45be669
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-10-21 12:15:23 +0100

    Added BpmMarking Component

---
+++ FinalYearProject.UI.Components/InterfaceElements/TopBar/BpmMarking.razor.cs
@@ -0,0 +1,32 @@
+using FinalYearProject.UI.Components.Models;
+using FinalYearProject.UI.Components.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace FinalYearProject.UI.Components.InterfaceElements.TopBar
+{
+    public partial class BpmMarking(TabRepresentationSettingsService settingsService)
+    {
+        public TabRepresentationSettingsService SettingsService { get; } = settingsService;
+
+        [Parameter, EditorRequired]
+        public required BarDisplayInformation BarInformation { get; set; }
+
+        #region Settings
+        private int _topPadding
+            => SettingsService
+            .TopBarDisplaySettings
+            .TopPadding;
+
+        private int _leftPadding
+            => SettingsService
+            .TopBarDisplaySettings
+            .LeftPadding;
+
+        private int _barNumberFontSize
+            => SettingsService
+            .TopBarDisplaySettings
+            .BarNumberFontSize;
+
+        #endregion
+    }
+}
