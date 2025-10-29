d174cc0c68f3114f5a690a8398fbc02456cba236
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-10-29 14:27:21 +0000

    Renamed CrotchetDuration to NoteDuration to match use case

--- FinalYearProject.UI.Components/InterfaceElements/BottomBar/NoteDuration/SeparateDuration.razor.cs
+++ FinalYearProject.UI.Components/InterfaceElements/BottomBar/NoteDuration/SeparateDuration.razor.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Components;
 
 namespace FinalYearProject.UI.Components.InterfaceElements.BottomBar.NoteDuration
 {
-    public partial class CrotchetDuration(TabRepresentationService representationService)
+    public partial class SeparateDuration(TabRepresentationService representationService)
     {
         #region Parameters
 
