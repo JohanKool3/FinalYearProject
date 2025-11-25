734bea746eb2708af9b2f0cebb65165dc6b271a6
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-11-25 20:47:40 +0000

    Defined audio pipeline interfaces

---
+++ FinalYearProject.Audio/Interfaces/IAudioFrameProvider.cs
@@ -0,0 +1,10 @@
+﻿namespace FinalYearProject.Audio.Interfaces
+{
+    /// <summary>
+    /// Defines a Component that converts audio data into frames for analysis.
+    /// </summary>
+    public interface IAudioFrameProvider
+    {
+        bool TryGetFrame(float[] buffer);
+    }
+}
---
+++ FinalYearProject.Audio/Interfaces/IAudioSource.cs
@@ -0,0 +1,28 @@
+﻿namespace FinalYearProject.Audio.Interfaces
+{
+    /// <summary>
+    /// An Interface for a Component that will return Audio Data.
+    /// </summary>
+    public interface IAudioSource : IAsyncDisposable
+    {
+        /// <summary>
+        /// The number of samples per second in Khz.
+        /// </summary>
+        public int SampleRate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the number of audio channels used by the instance.
+        /// </summary>
+        /// <remarks>A value of 1 typically indicates mono audio, while 2 indicates stereo</remarks>
+        public int Channels { get; set; }
+
+        /// <summary>
+        /// Fill the buffer with the next block of samples.
+        /// Returns the number of samples actually written.        
+        /// </summary>
+        /// <param name="buffer">Variable that will store the next 'chunk' of 
+        /// audio data</param>
+        /// <returns></returns>
+        public int Read(float[] buffer);
+    }
+}
