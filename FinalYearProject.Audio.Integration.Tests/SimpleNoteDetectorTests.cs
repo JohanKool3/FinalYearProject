0ebf701c7e8361657d985bef1e5eb4c790817d48
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2026-01-26 14:45:50 +0000

    Added multiple window detection test

--- FinalYearProject.Audio.Integration.Tests/SimpleNoteDetectorTests.cs
+++ FinalYearProject.Audio.Integration.Tests/SimpleNoteDetectorTests.cs
@@ -11,7 +11,7 @@ namespace FinalYearProject.Audio.Integration.Tests
     {
 
         [Fact]
-        public void SimpleNoteDetectorTests_C4SineWave_ReturnsC4NoteDetection()
+        public void SimpleNoteDetectorTests_SingleC4SineWaveWindow_ReturnsC4NoteDetection()
         {
             // Arrange
             var tuningScheme = new TuningScheme()
@@ -68,6 +68,78 @@ namespace FinalYearProject.Audio.Integration.Tests
                 .OrderByDescending(nc => nc.Confidence)
                 .First()
                 .Name);
+
+        }
+
+        [Fact]
+        public void SimpleNoteDetectorTests_MultipleC4SineWaveWindow_ReturnsC4NoteDetection()
+        {
+            // Arrange
+            var tuningScheme = new TuningScheme()
+            {
+                A4 = 440.0
+            };
+
+            var reader = new WavFileReader();
+            var fileName = "TestData\\pure-sine-c4.wav";
+            var windower = new SimpleWindower();
+            var fftAnalyzer = new FftAnalyzer();
+            var timelineConstructor = new SimpleFrequencyTimelineConstructor();
+            var noteDetector = new SimpleNoteDetector(tuningScheme);
+
+            // Act
+
+            // Read
+            var pcmStream = reader.ReadAudioFile(fileName);
+
+            // Chunk
+            var windows = windower.ConvertPCMStreamToWindows
+                (pcmStream, 44100, 4096, 2048);
+
+            // Convert Chunks to Frequency Domain Snapshots
+            List<FftOutput> fftOutputs = [];
+
+            foreach (var window in windows)
+            {
+                var fftOutput = fftAnalyzer.ConvertToFrequencyDomain(window, 44100);
+
+                fftOutputs.Add(fftOutput);
+            }
+
+            // Construct Timeline from Frequency Domain Snapshots
+            var timeline = timelineConstructor.GenerateTimeline(fftOutputs);
+
+            var frequencyTotals = new Dictionary<double, double>();
+
+            foreach (var frequency in timeline.FrequencyMagnitude.Keys)
+            {
+
+                frequencyTotals[frequency] = timeline.FrequencyMagnitude[frequency].Sum();
+            }
+
+            // Take first Snapshot from the Timeline
+            var frequencies = timeline.Frequencies;
+
+
+            var allNoteSlices = new List<NoteSlice>();
+
+            foreach (var window in timeline.FrequencyMagnitudeWindows)
+            {
+
+                var noteSlice = noteDetector.CalculateNoteConfidenceValues
+                    (window, frequencies, 0.0);
+
+                allNoteSlices.Add(noteSlice);
+            }
+
+            // Assert
+            foreach (var noteSlice in allNoteSlices)
+            {
+                Assert.Equal("C5", noteSlice.NoteConfidences
+                    .OrderByDescending(nc => nc.Confidence)
+                    .First()
+                    .Name);
+            }
 
         }
     }
