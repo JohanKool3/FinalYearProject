2880db8c5caf82f7c8ad98436e8c0083452748ac
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2026-02-04 14:53:16 +0000

    Added Loading Icon

---
+++ FinalYearProject.UI.Components/InterfaceElements/TabBrowser/Elements/BrowserLoadingIcon.razor.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Components;
+
+namespace FinalYearProject.UI.Components.InterfaceElements.TabBrowser.Elements
+{
+    public partial class BrowserLoadingIcon
+    {
+
+        [Parameter]
+        public int Width { get; set; } = 64;
+
+        [Parameter]
+        public int Height { get; set; } = 64;
+    }
+}
