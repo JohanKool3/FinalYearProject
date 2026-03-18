346e1a63809a5329b297e1323878e57e9e9cb2f1
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2026-03-18 13:22:08 +0000

    Added extra information for testing

--- FinalYearProject.Server/Seeding/PieceDataHelper.cs
+++ FinalYearProject.Server/Seeding/PieceDataHelper.cs
@@ -1,4 +1,6 @@
 ﻿using FinalYearProject.EfCore.Models;
+using FinalYearProject.Shared.Enums;
+using FinalYearProject.Shared.Models.TabRepresentation;
 
 namespace FinalYearProject.Server.Seeding
 {
@@ -12,7 +14,14 @@ namespace FinalYearProject.Server.Seeding
         {
             List<PieceModel> output = [];
 
-            var newPiece = new PieceModel()
+            var standardTimeSignature = new TimeSignatureModel()
+            {
+                BeatsPerMeasure = 4,
+                BeatUnit = 4
+            };
+
+            // C Major Scale
+            var newPiece_1 = new PieceModel()
             {
                 Id = Guid.Empty,
                 TabInformationModel = new()
@@ -189,11 +198,258 @@ namespace FinalYearProject.Server.Seeding
                         }
                     ]
                 },
-                
-                
+
+
             };
 
-            output.Add(newPiece);
+            var newPiece_2 = new PieceModel()
+            {
+                Id = Guid.NewGuid(),
+                TabInformationModel = new()
+                {
+                    Title = "Master of Puppets",
+                    Author = "Metallica",
+                    Description = "All down strokes",
+                    TotalLengthInSeconds = 4.5283f,
+                    Bars = [
+                        // First Bar
+                        new(){
+                            Bpm = 212,
+                            TimeSignature = standardTimeSignature,
+                            Position = new(){
+                                StartTime = 0,
+                                EndTime = 1.132f,
+                                Length = 2.264f
+                            },
+                            NoteGroups = [
+                                new(){
+                                    Chords = [],
+                                    BarStartPercentage = 0,
+                                    BarEndPercentage = 50,
+                                    TotalGroupBeatLength = 2,
+                                    Notes = [
+                                        new(){
+                                            StartPercentage = 0,
+                                            EndPercentage = 25,
+                                            FretNumber = 0,
+                                            StringNumber = 6,
+                                            Properties = new(){
+                                                Articulation = ArticulationType.PalmMute,
+                                                Length = 0.5f,
+                                                Type = NoteType.Normal,
+                                                IsGrouped = true
+                                            }
+                                        },
+                                        new(){
+                                            StartPercentage = 25,
+                                            EndPercentage = 50,
+                                            FretNumber = 0,
+                                            StringNumber = 6,
+                                            Properties = new(){
+                                                Articulation = ArticulationType.PalmMute,
+                                                Length = 0.5f,
+                                                Type = NoteType.Normal,
+                                                IsGrouped = true
+                                            }
+                                        },
+                                        new(){
+                                            StartPercentage = 50,
+                                            EndPercentage = 75,
+                                            FretNumber = 12,
+                                            StringNumber = 6,
+                                            Properties = new(){
+                                                Articulation = ArticulationType.None,
+                                                Length = 0.5f,
+                                                Type = NoteType.Normal,
+                                                IsGrouped = true
+                                            }
+                                        },
+                                        new(){
+                                            StartPercentage = 75,
+                                            EndPercentage = 100,
+                                            FretNumber = 0,
+                                            StringNumber = 6,
+                                            Properties = new(){
+                                                Articulation = ArticulationType.PalmMute,
+                                                Length = 0.5f,
+                                                Type = NoteType.Normal,
+                                                IsGrouped = true
+                                            }
+                                        }
+                                        ]
+
+                                },
+                                new(){
+                                    Chords = [],
+                                    BarStartPercentage = 50,
+                                    BarEndPercentage = 100,
+                                    TotalGroupBeatLength = 2,
+                                    Notes = [
+                                        new(){
+                                            StartPercentage = 0,
+                                            EndPercentage = 25,
+                                            FretNumber = 0,
+                                            StringNumber = 6,
+                                            Properties = new(){
+                                                Articulation = ArticulationType.PalmMute,
+                                                Length = 0.5f,
+                                                Type = NoteType.Normal,
+                                                IsGrouped = true
+                                            }
+                                        },
+                                        new(){
+                                            StartPercentage = 25,
+                                            EndPercentage = 50,
+                                            FretNumber = 11,
+                                            StringNumber = 6,
+                                            Properties = new(){
+                                                Articulation = ArticulationType.None,
+                                                Length = 0.5f,
+                                                Type = NoteType.Normal,
+                                                IsGrouped = true
+                                            }
+                                        },
+                                        new(){
+                                            StartPercentage = 50,
+                                            EndPercentage = 75,
+                                            FretNumber = 0,
+                                            StringNumber = 6,
+                                            Properties = new(){
+                                                Articulation = ArticulationType.PalmMute,
+                                                Length = 0.5f,
+                                                Type = NoteType.Normal,
+                                                IsGrouped = true
+                                            }
+                                        },
+                                        new(){
+                                            StartPercentage = 75,
+                                            EndPercentage = 100,
+                                            FretNumber = 0,
+                                            StringNumber = 6,
+                                            Properties = new(){
+                                                Articulation = ArticulationType.PalmMute,
+                                                Length = 0.5f,
+                                                Type = NoteType.Normal,
+                                                IsGrouped = true
+                                            }
+                                        }
+                                        ]
+
+                                },]
+
+                        },
+                        // Second Bar
+                        new(){
+                            Bpm = 212,
+                            TimeSignature = standardTimeSignature,
+                            Position = new(){
+                                StartTime = 1.132f,
+                                EndTime = 2.264f,
+                                Length = 1.132f
+                            },
+                            NoteGroups = [
+                                new(){
+                                    Chords = [],
+                                    BarStartPercentage = 0,
+                                    BarEndPercentage = 25,
+                                    TotalGroupBeatLength = 1,
+                                    Notes = [
+                                        new(){
+                                            StartPercentage = 0,
+                                            EndPercentage = 100,
+                                            StringNumber = 6,
+                                            FretNumber = 10,
+                                            Properties = new(){
+                                                Articulation = ArticulationType.None,
+                                                IsGrouped = false,
+                                                Length = 1,
+                                                Type = NoteType.Normal
+                                            }
+                                        },
+                                        new(){
+                                            StartPercentage = 0,
+                                            EndPercentage = 100,
+                                            StringNumber = 5,
+                                            FretNumber = 12,
+                                            Properties = new(){
+                                                Articulation = ArticulationType.None,
+                                                IsGrouped = false,
+                                                Length = 1,
+                                                Type = NoteType.Normal
+                                            }
+                                        }
+                                        ]
+                                },
+                                new(){
+                                    Chords = [],
+                                    BarStartPercentage = 25,
+                                    BarEndPercentage = 50,
+                                    TotalGroupBeatLength = 1,
+                                    Notes = [
+                                        new(){
+                                            StartPercentage = 0,
+                                            EndPercentage = 100,
+                                            StringNumber = 6,
+                                            FretNumber = 9,
+                                            Properties = new(){
+                                                Articulation = ArticulationType.None,
+                                                IsGrouped = false,
+                                                Length = 1,
+                                                Type = NoteType.Normal
+                                            }
+                                        },
+                                        new(){
+                                            StartPercentage = 0,
+                                            EndPercentage = 100,
+                                            StringNumber = 5,
+                                            FretNumber = 11,
+                                            Properties = new(){
+                                                Articulation = ArticulationType.None,
+                                                IsGrouped = false,
+                                                Length = 1,
+                                                Type = NoteType.Normal
+                                            }
+                                        }
+                                        ]
+                                },
+                            new(){
+                                    Chords = [],
+                                    BarStartPercentage = 50,
+                                    BarEndPercentage = 100,
+                                    TotalGroupBeatLength = 2,
+                                    Notes = [
+                                        new(){
+                                            StartPercentage = 0,
+                                            EndPercentage = 100,
+                                            StringNumber = 6,
+                                            FretNumber = 8,
+                                            Properties = new(){
+                                                Articulation = ArticulationType.None,
+                                                IsGrouped = false,
+                                                Length = 1,
+                                                Type = NoteType.Normal
+                                            }
+                                        },
+                                        new(){
+                                            StartPercentage = 0,
+                                            EndPercentage = 100,
+                                            StringNumber = 5,
+                                            FretNumber = 10,
+                                            Properties = new(){
+                                                Articulation = ArticulationType.None,
+                                                IsGrouped = false,
+                                                Length = 1,
+                                                Type = NoteType.Normal
+                                            }
+                                        }
+                                        ]
+                                },]
+                    }]
+                },
+            };
+
+            output.Add(newPiece_1);
+            output.Add(newPiece_2);
 
             return output;
         }
